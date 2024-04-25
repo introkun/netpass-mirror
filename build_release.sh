8b0c01e3d99e58f60b652c5321c86958ef6b3806
cooolgamer <64099608+cooolgamer@users.noreply.github.com>
2024-04-25 17:37:40 +0200

    Added cia build to Makefile and added dummy banner

--- build_release.sh
+++ build_release.sh
@@ -5,14 +5,15 @@ make
 source version.env
 
 # allows easier upload
-cp netpass.3dsx netpass.3dsx.txt
+cp out/netpass.3dsx out/netpass.3dsx.txt
 
 # strip elf
 $DEVKITARM/bin/arm-none-eabi-strip netpass.elf
 
-makerom -f cia -v -target t -exefslogo -o netpass.cia \
-	-elf netpass.elf -rsf netpass.rsf \
+makerom -f cia -v -target t -exefslogo -o out/netpass.cia \
+	-elf out/netpass.elf -rsf meta/netpass.rsf \
 	-major $NETPASS_VERSION_MAJOR -minor $NETPASS_VERSION_MINOR -micro $NETPASS_VERSION_MICRO \
-	-icon netpass.smdh
+	-icon meta/netpass.smdh \
+	-banner meta/banner.bnr
 
-cp netpass.cia netpass.cia.txt
+cp out/netpass.cia out/netpass.cia.txt
