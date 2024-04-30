add1038c4094807b927f758e2901bf4db0fdd91b
Sorunome <+@sorunome.de>
2024-04-30 10:56:13 +0200

    Add license

--- server/server.py
+++ server/server.py
@@ -1,3 +1,19 @@
+# NetPass
+# Copyright (C) 2024 Sorunome
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from raw_types import RawMessage, MAX_MESSAGE_SIZE, MBOXLIST_SIZE, MBoxList
 from threading import Timer
