46867e7f4969d0531071214283d260d816b02c42
SunOfLife1 <yfitzge@gmail.com>
2024-05-23 18:28:46 -0400

    Add Toggle Titles option to Settings
    - Creates new files for toggle_titles scene
    - Includes header for toggle_titles scene in scene.h
    - Makes proper modifications to scenes/settings.c to display the new "Toggle Titles" option
    
    The "Toggle Titles" option will currently crash the 3DS, so don't... don't click it

---
+++ source/scenes/toggle_titles.c
@@ -0,0 +1,26 @@
+/**
+ * NetPass
+ * Copyright (C) 2024 Sorunome
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "toggle_titles.h"
+#include <stdlib.h>
+
+#define N(x) scenes_toggle_titles_namespace_##x
+#define _data ((N(DataStruct)*)sc->d)
+
+Scene* getToggleTitlesScene(void) {
+}
