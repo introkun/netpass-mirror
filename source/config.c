0bdfde28b09ebd03c26b9e3fd2f593cb74f50b35
SunOfLife1 <yfitzge@gmail.com>
2024-05-27 08:54:24 -0400

    Add MY Display Name to Headers
    - In scenes/toggle_titles.c and scenes/toggle_titles.h, replaces Soru's name in the header with mine
    - In config.c, adds my name

--- source/config.c
+++ source/config.c
@@ -1,6 +1,6 @@
 /**
  * NetPass
- * Copyright (C) 2024 Sorunome
+ * Copyright (C) 2024 Sorunome, SunOfLife1
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
