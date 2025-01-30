78e18515d584fb41937f80615ec52eeed05c469b
Sorunome <+@sorunome.de>
2025-01-30 20:24:50 +0100

    fix copyright headers

--- source/report.h
+++ source/report.h
@@ -1,6 +1,6 @@
 /**
  * NetPass
- * Copyright (C) 2024 Sorunome
+ * Copyright (C) 2024-2025 Sorunome
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
