introkun/netpass-mirror

commit 7220b88f796fc5836f80b7f96bf08770707b5805

- fixed review notes

source/main.c
@@ -50,8 +50,8 @@ int main() {
 	DEBUG_PRINTF("DEBUG ON\n");
 
 	cecdInit();
-	Result curlInitResult = curlInit();
-	if (R_FAILED(curlInitResult)) {
+	Result res = curlInit();
+	if (R_FAILED(res)) {
 		DEBUG_PRINTF("Curl initialization failed\n");
 	}
 	srand(time(NULL));
@@ -108,7 +108,6 @@ int main() {
 				}
 				return getLocationScene(location);
 			})), lambda(void, (void) {
-				Result res;
 				// first, we import the locally stored passes for reports to work
 				reportInit();
 				// next, we gotta wait for having internet

source/debug.h
@@ -1,6 +1,6 @@
 /**
  * NetPass
- * Copyright (C) 2024-2025 Introkun
+ * Copyright (C) 2025 Introkun
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,7 +21,7 @@
 #ifdef DEBUG
     #include <stdio.h>
     #define DEBUG_PRINTF(...) \
-        do { fprintf(stderr, "[DEBUG] "); fprintf(stderr, __VA_ARGS__); } while (0)
+        do { printf("[DEBUG] "); printf(__VA_ARGS__); } while (0)
 #else
     #define DEBUG_PRINTF(...) ((void)0)
 #endif

commit 7b9974b2d63ab03bfb7333434c6f1a0670f863b5

Fixed app crash introduced by 7220b88f796fc5836f80b7f96bf08770707b5805

source/main.c
@@ -108,6 +108,7 @@ int main() {
 				}
 				return getLocationScene(location);
 			})), lambda(void, (void) {
+				Result res;
 				// first, we import the locally stored passes for reports to work
 				reportInit();
 				// next, we gotta wait for having internet