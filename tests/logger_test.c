d88988bee08c0e055ed490e8a6800a1e97b2c45f
sergey.gradovich <sergey.gradovich@gmail.com>
2025-07-20 11:44:28 -0300

    added more logger tests

--- tests/logger_test.c
+++ tests/logger_test.c
@@ -20,8 +20,6 @@ static void assert_log_contains(const int expectedDebug, const int expectedInfo,
     FILE* f = fopen(TEST_LOG_PATH, "r");
     assert(f && "Failed to open log file");
 
-    rewind(f);
-
     while (fgets(line, sizeof(line), f)) {
         if (strstr(line, "[DEBUG]")) foundDebug++;
         if (strstr(line, "[INFO]"))  foundInfo++;
@@ -30,85 +28,73 @@ static void assert_log_contains(const int expectedDebug, const int expectedInfo,
     }
     fclose(f);
 
-    printf("foundDebug %d, expectedDebug %d\n", foundDebug, expectedDebug);
+    //printf("foundDebug %d, expectedDebug %d\n", foundDebug, expectedDebug);
     assert(foundDebug == expectedDebug && "Unexpected number of debug log entries");
     assert(foundInfo  == expectedInfo  && "Unexpected number of info log entries");
     assert(foundWarn  == expectedWarn && "Unexpected number of warn log entries");
     assert(foundError == expectedError && "Unexpected number of error log entries");
 }
 
+static void write_logs() {
+    logDebug("debug message\n");
+    logInfo("info message\n");
+    logWarn("warn message\n");
+    logError("error message\n");
+}
+
+#define RUN_TEST(fn, expectedD, expectedI, expectedW, expectedE) do { \
+    truncate_log_file(); \
+    fn(); \
+    assert_log_contains(expectedD, expectedI, expectedW, expectedE); \
+    printf("[PASS] %s\n", #fn); \
+} while (0)
+
+#define RUN_TEST_WITH_1_PARAM(fn, param, expectedD, expectedI, expectedW, expectedE) do { \
+    truncate_log_file(); \
+    fn(param); \
+    assert_log_contains(expectedD, expectedI, expectedW, expectedE); \
+    printf("[PASS] %s(%d)\n", #fn, param); \
+} while (0)
+
 static void test_logger_file_write() {
     loggerInit(LOG_LEVEL_DEBUG, LOG_OUTPUT_FILE, LOG_FILE_PATH);
     logDebug("debug message");
     logInfo("info message");
     loggerClose();
-    printf("[PASS] test_logger_file_write\n");
 }
 
 static void test_logger_debug() {
     loggerInit(LOG_LEVEL_DEBUG, LOG_OUTPUT_FILE, LOG_FILE_PATH);
-    logDebug("debug message");
-    logInfo("info message");
-    logWarn("warn message");
-    logError("error message");
+    write_logs();
 
     loggerClose();
-    printf("[PASS] test_logger_debug\n");
 }
 
 static void test_logger_info() {
     loggerInit(LOG_LEVEL_INFO, LOG_OUTPUT_FILE, LOG_FILE_PATH);
-    logDebug("debug message");
-    logInfo("info message");
-    logWarn("warn message");
-    logError("error message");
+    write_logs();
 
     loggerClose();
-    printf("[PASS] test_logger_info\n");
 }
 
 static void test_logger_warn() {
     loggerInit(LOG_LEVEL_WARN, LOG_OUTPUT_FILE, LOG_FILE_PATH);
-    logDebug("debug message");
-    logInfo("info message");
-    logWarn("warn message");
-    logError("error message");
+    write_logs();
 
     loggerClose();
-    printf("[PASS] test_logger_warn\n");
 }
 
 static void test_logger_error() {
     loggerInit(LOG_LEVEL_ERROR, LOG_OUTPUT_FILE, LOG_FILE_PATH);
-    logDebug("debug message");
-    logInfo("info message");
-    logWarn("warn message");
-    logError("error message");
+    write_logs();
 
     loggerClose();
-    printf("[PASS] test_logger_error\n");
 }
 
-static void test_logger_fallback_to_info_for_incorrect_level_1() {
-    loggerInit(10, LOG_OUTPUT_FILE, LOG_FILE_PATH);
-    logDebug("debug message");
-    logInfo("info message");
-    logWarn("warn message");
-    logError("error message");
-
+static void test_logger_fallback_to_info_for_incorrect_level(int level) {
+    loggerInit(level, LOG_OUTPUT_FILE, LOG_FILE_PATH);
+    write_logs();
     loggerClose();
-    printf("[PASS] test_logger_fallback_to_info_for_incorrect_level_1\n");
-}
-
-static void test_logger_fallback_to_info_for_incorrect_level_2() {
-    loggerInit(-10, LOG_OUTPUT_FILE, LOG_FILE_PATH);
-    logDebug("debug message");
-    logInfo("info message");
-    logWarn("warn message");
-    logError("error message");
-
-    loggerClose();
-    printf("[PASS] test_logger_fallback_to_info_for_incorrect_level_2\n");
 }
 
 static void test_logger_empty_file_output() {
@@ -117,102 +103,43 @@ static void test_logger_empty_file_output() {
     loggerInit(LOG_LEVEL_DEBUG, LOG_OUTPUT_FILE, NULL);  // Disable output
     logInfo("info message");
     loggerClose();
-    printf("[PASS] test_logger_empty_file_output\n");
 }
 
-static void test_logger_fallback_to_screen_for_incorrect_output_1() {
-    loggerInit(LOG_LEVEL_DEBUG, 10, LOG_FILE_PATH);
-    logDebug("debug message");
-    logInfo("info message");
-    logWarn("warn message");
-    logError("error message");
-
+static void test_logger_fallback_to_screen_for_incorrect_output(int output) {
+    loggerInit(LOG_LEVEL_DEBUG, output, LOG_FILE_PATH);
+    write_logs();
     loggerClose();
-    printf("[PASS] test_logger_fallback_to_screen_for_incorrect_output_1\n");
-}
-
-static void test_logger_fallback_to_screen_for_incorrect_output_2() {
-    loggerInit(LOG_LEVEL_DEBUG, -10, LOG_FILE_PATH);
-    logDebug("debug message");
-    logInfo("info message");
-    logWarn("warn message");
-    logError("error message");
-
-    loggerClose();
-    printf("[PASS] test_logger_fallback_to_screen_for_incorrect_output_2\n");
 }
 
 static void test_logger_level_both_logs_to_file() {
     loggerInit(LOG_LEVEL_DEBUG, LOG_OUTPUT_BOTH, LOG_FILE_PATH);
-    logDebug("debug message");
-    logInfo("info message");
-    logWarn("warn message");
-    logError("error message");
+    write_logs();
 
     loggerClose();
-    printf("[PASS] test_logger_level_both_logs_to_file\n");
 }
 
 static void test_logger_level_screen_doesnt_log_to_file() {
     loggerInit(LOG_LEVEL_DEBUG, LOG_OUTPUT_SCREEN, LOG_FILE_PATH);
-    logDebug("debug message");
-    logInfo("info message");
-    logWarn("warn message");
-    logError("error message");
+    write_logs();
 
     loggerClose();
-    printf("[PASS] test_logger_level_screen_doesnt_log_to_file\n");
 }
 
 int main() {
-    truncate_log_file();
-    test_logger_file_write();
-    assert_log_contains(1, 1, 0, 0);
+    RUN_TEST(test_logger_file_write, 1, 1, 0, 0);
+    RUN_TEST(test_logger_empty_file_output, 0, 2, 0, 0);
+    RUN_TEST(test_logger_debug, 1, 1, 1, 1);
+    RUN_TEST(test_logger_info, 0, 1, 1, 1);
+    RUN_TEST(test_logger_warn, 0, 0, 1, 1);
+    RUN_TEST(test_logger_error, 0, 0, 0, 1);
 
-    truncate_log_file();
-    test_logger_empty_file_output();
-    assert_log_contains(0, 2, 0, 0);
+    RUN_TEST_WITH_1_PARAM(test_logger_fallback_to_info_for_incorrect_level, 10, 0, 1, 1, 1);
+    RUN_TEST_WITH_1_PARAM(test_logger_fallback_to_info_for_incorrect_level, -10, 0, 1, 1, 1);
+    RUN_TEST_WITH_1_PARAM(test_logger_fallback_to_screen_for_incorrect_output, 10, 0, 0, 0, 0);
+    RUN_TEST_WITH_1_PARAM(test_logger_fallback_to_screen_for_incorrect_output, -10, 0, 0, 0, 0);
 
-    truncate_log_file();
-    test_logger_debug();
-    assert_log_contains(1, 1, 1, 1);
-
-    truncate_log_file();
-    test_logger_info();
-    assert_log_contains(0, 1, 1, 1);
-
-    truncate_log_file();
-    test_logger_warn();
-    assert_log_contains(0, 0, 1, 1);
-
-    truncate_log_file();
-    test_logger_error();
-    assert_log_contains(0, 0, 0, 1);
-
-    truncate_log_file();
-    test_logger_fallback_to_info_for_incorrect_level_1();
-    assert_log_contains(0, 1, 1, 1);
-
-    truncate_log_file();
-    test_logger_fallback_to_info_for_incorrect_level_2();
-    assert_log_contains(0, 1, 1, 1);
-
-    truncate_log_file();
-    assert_log_contains(0, 0, 0, 0);
-    test_logger_fallback_to_screen_for_incorrect_output_1();
-    assert_log_contains(0, 0, 0, 0);
-
-    truncate_log_file();
-    test_logger_fallback_to_screen_for_incorrect_output_2();
-    assert_log_contains(0, 0, 0, 0);
-
-    truncate_log_file();
-    test_logger_level_both_logs_to_file();
-    assert_log_contains(1, 1, 1, 1);
-
-    truncate_log_file();
-    test_logger_level_screen_doesnt_log_to_file();
-    assert_log_contains(0, 0, 0, 0);
+    RUN_TEST(test_logger_level_both_logs_to_file, 1, 1, 1, 1);
+    RUN_TEST(test_logger_level_screen_doesnt_log_to_file, 0, 0, 0, 0);
 
     printf("All tests passed!\n");
     return 0;
